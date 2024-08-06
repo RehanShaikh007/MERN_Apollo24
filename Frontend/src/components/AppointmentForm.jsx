c284515a95918a2bbc56f328e19f294c9a15fd79
RehanShaikh007 <rehanmukhtarshaikh7@gmail.com>
2024-08-06 13:01:23 +0530

    Updated

--- Frontend/src/components/AppointmentForm.jsx
+++ Frontend/src/components/AppointmentForm.jsx
@@ -38,7 +38,7 @@ const AppointmentForm = () => {
   useEffect(() => {
     const fetchDoctors = async () => {
       const { data } = await axios.get(
-        "http://localhost:4000/api/v1/user/doctors",
+        "https://mern-apollo24.onrender.com/api/v1/user/doctors",
         { withCredentials: true }
       );
       setDoctors(data.doctors);
@@ -51,7 +51,7 @@ const AppointmentForm = () => {
     try {
       const hasVisitedBool = Boolean(hasVisited);
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/appointment/post",
+        "https://mern-apollo24.onrender.com/api/v1/appointment/post",
         {
           firstName,
           lastName,
