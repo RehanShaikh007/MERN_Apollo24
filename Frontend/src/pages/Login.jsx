0398e3fc847b5b8697b884a8d1eaae8b45527c9e
Rehan Shaikh <143618117+RehanShaikh007@users.noreply.github.com>
2025-04-23 10:45:18 +0530

    Update Login.jsx

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     try {
       await axios
         .post(
-          "http://localhost:4000/api/v1/user/login",
+          "https://mern-apollo24-1.onrender.com/api/v1/user/login",
           { email, password, confirmPassword, role: "Patient" },
           {
             withCredentials: true,
