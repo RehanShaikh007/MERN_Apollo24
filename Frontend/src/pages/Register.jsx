53f38e25fec1bdaab0918e802f0861ec6541f9e9
Rehan Shaikh <143618117+RehanShaikh007@users.noreply.github.com>
2025-04-23 10:45:39 +0530

    Update Register.jsx

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -22,7 +22,7 @@ const Register = () => {
     e.preventDefault();
     try {
       const response= await axios.post(
-          "http://localhost:4000/api/v1/user/patient/register",
+          "https://mern-apollo24-1.onrender.com/api/v1/user/patient/register",
           { firstName, lastName, email, phone, aadhar, dob, gender, password, role: "Patient", },
           {
             withCredentials: true,
